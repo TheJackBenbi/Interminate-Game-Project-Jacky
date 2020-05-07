af0ec80ecfffb707e66d90e6179d527da3592950
TheJackBenbi <jackywang3032668@gmail.com>
2020-05-07 11:13:58 +0930

    new updates on thursday
    -added some animation to serve as the parkcord
    -third person controller added
    -added basic structure

---
+++ New Unity Project (4)/Assets/_scripts/Open_Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Door : MonoBehaviour
+{
+    public Animator doorOpen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (doorOpen != null)
+        {
+            doorOpen.SetTrigger("open door trigger");
+        }
+    }
+}
